db3f09eb17075cfa4bb087c6abce9253358f2e98
olivier <paul.olivier@eurecom.fr>
2022-09-21 11:15:47 +0200

    Fix unstuff binwalk's dependency broken URL

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ sudo apt-get install -y busybox-static bash-static fakeroot dmsetup kpartx netca
 wget https://github.com/ReFirmLabs/binwalk/archive/refs/tags/v2.3.3.tar.gz && \
   tar -xf v2.3.3.tar.gz && \
   cd binwalk-2.3.3 && \
+  sed -i 's/sdtraces/sdtraces\/BottinHTML/g' deps.sh && \
   echo y | ./deps.sh && \
   sudo python3 setup.py install
 sudo apt-get install -y mtd-utils gzip bzip2 tar arj lhasa p7zip p7zip-full cabextract fusecram cramfsswap squashfs-tools sleuthkit default-jdk cpio lzop lzma srecord zlib1g-dev liblzma-dev liblzo2-dev unzip
